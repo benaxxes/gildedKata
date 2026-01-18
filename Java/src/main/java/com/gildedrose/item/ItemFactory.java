0ccf745f453a0c8a88846f366ef855aceaa0a807
alen <benalen5@gmail.com>
2026-01-18 10:37:08 +0100

    simplify factory by moving all item type knowledge to the enum

--- Java/src/main/java/com/gildedrose/item/ItemFactory.java
+++ Java/src/main/java/com/gildedrose/item/ItemFactory.java
@@ -4,35 +4,29 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ItemFactory {
     private static final String CONJURED_PREFIX = "Conjured";
 
     public static ItemUpdater newItem(Item item) {
-        //todo: validate item
+        validate(item);
 
         return Arrays.stream(ItemType.values())
             .filter(type -> type.getName().equals(item.name))
             .findFirst()
-            .map(type -> mapItem(item, type))
-            .orElseGet(() -> {
-                if (isConjured(item)) {
-                    return new ConjuredItem(item);
-                }
-                return new NormalItem(item);
-            });
+            .map(type -> type.create(item))
+            .orElseGet(() -> isConjured(item) ? new ConjuredItem(item) : new NormalItem(item));
     }
 
-    private static ItemUpdater mapItem(Item item, ItemType type) {
-        return switch (type) {
-            case AGED_BRIE -> new AgedBrie(item);
-            case BACKSTAGE_PASSES -> new BackstagePass(item);
-            case SULFURAS -> new Sulfuras(item);
-        };
+    private static void validate(Item item) {
+        Objects.requireNonNull(item, "Item cannot be null");
+        Objects.requireNonNull(item.name, "Item name cannot be null");
     }
 
     private static boolean isConjured(Item item) {
         return item.name.startsWith(CONJURED_PREFIX);
     }
+
 }
--- Java/src/main/java/com/gildedrose/item/ItemType.java
+++ Java/src/main/java/com/gildedrose/item/ItemType.java
@@ -1,17 +1,22 @@
 package com.gildedrose.item;
 
+import lombok.Getter;
+
+import java.util.function.Function;
+
 public enum ItemType {
-    AGED_BRIE("Aged Brie"),
-    BACKSTAGE_PASSES("Backstage passes to a TAFKAL80ETC concert"),
-    SULFURAS("Sulfuras, Hand of Ragnaros");
+    AGED_BRIE("Aged Brie", AgedBrie::new),
+    BACKSTAGE_PASSES("Backstage passes to a TAFKAL80ETC concert", BackstagePass::new),
+    SULFURAS("Sulfuras, Hand of Ragnaros", Sulfuras::new);
 
-    final String name;
+    @Getter
+    private final String name;
+    private final Function<Item, ItemUpdater> factory;
 
-    ItemType(String name) {
+    ItemType(String name, Function<Item, ItemUpdater> factory) {
         this.name = name;
+        this.factory = factory;
     }
 
-    public String getName() {
-        return name;
-    }
+    public ItemUpdater create(Item item) { return factory.apply(item); }
 }
