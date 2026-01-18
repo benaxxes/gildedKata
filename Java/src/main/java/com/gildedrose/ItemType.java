cdb47b4cf0339e52bd446a926df5d1c09e179cdf
alen <benalen5@gmail.com>
2026-01-18 09:20:34 +0100

    use enum for itemTypes

---
+++ Java/src/main/java/com/gildedrose/ItemType.java
@@ -0,0 +1,17 @@
+package com.gildedrose;
+
+public enum ItemType {
+    AGED_BRIE("Aged Brie"),
+    BACKSTAGE_PASSES("Backstage passes to a TAFKAL80ETC concert"),
+    SULFURAS("Sulfuras, Hand of Ragnaros");
+
+    final String name;
+
+    ItemType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,41 +6,11 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static com.gildedrose.ItemType.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GildedRoseTest {
 
-
-    private static Stream<Arguments> provideSpecialItems() {
-        return Stream.of(
-            Arguments.of("Aged Brie", 1, 9, 21),
-            Arguments.of("Aged Brie", 10, 0, 30),
-            Arguments.of("Aged Brie", 11, -1, 32),
-            Arguments.of("Aged Brie", 100, -90, 50),
-
-            // Sulfuras never has to be sold and never decreases in Quality
-            Arguments.of("Sulfuras, Hand of Ragnaros", 1, 10, 20),
-            Arguments.of("Sulfuras, Hand of Ragnaros", 10, 10, 20),
-
-
-            Arguments.of("Backstage passes to a TAFKAL80ETC concert", 1, 9, 22),
-            Arguments.of("Backstage passes to a TAFKAL80ETC concert", 6, 4, 33),
-            Arguments.of("Backstage passes to a TAFKAL80ETC concert", 11, -1, 0)
-        );
-    }
-
-    private static Stream<Arguments> provideTimesAndExpectedQualityAndSellIn() {
-        return Stream.of(
-            Arguments.of(1, 9, 19),
-            Arguments.of(2, 8, 18),
-            Arguments.of(10, 0, 10),
-            // quality degrades twice as fast after sellIn date
-            Arguments.of(11, -1, 8),
-            // quality should not go negative
-            Arguments.of(20, -10, 0),
-            Arguments.of(25, -15, 0)
-        );
-    }
 
     @ParameterizedTest(name = "After {0} days, sellIn should be {1} and quality should be {2}")
     @MethodSource("provideTimesAndExpectedQualityAndSellIn")
@@ -59,9 +29,9 @@ class GildedRoseTest {
 
     @ParameterizedTest(name = "Item: \"{0}\" - After {1} days, sellIn should be {2} and quality should be {3}")
     @MethodSource("provideSpecialItems")
-    void specialItemUpdateTest(String itemName, int times, int expectedSellIn, int expectedQuality) {
+    void specialItemUpdateTest(ItemType itemName, int times, int expectedSellIn, int expectedQuality) {
         // given
-        Item[] items = new Item[]{new Item(itemName, 10, 20)};
+        Item[] items = new Item[]{new Item(itemName.getName(), 10, 20)};
         GildedRose gildedRose = new GildedRose(items);
 
         // when
@@ -76,6 +46,37 @@ class GildedRoseTest {
         for (int i = 0; i < times; i++) {
             gildedRose.updateQuality();
         }
+    }
+
+    private static Stream<Arguments> provideTimesAndExpectedQualityAndSellIn() {
+        return Stream.of(
+            Arguments.of(1, 9, 19),
+            Arguments.of(2, 8, 18),
+            Arguments.of(10, 0, 10),
+            // quality degrades twice as fast after sellIn date
+            Arguments.of(11, -1, 8),
+            // quality should not go negative
+            Arguments.of(20, -10, 0),
+            Arguments.of(25, -15, 0)
+        );
+    }
+
+    private static Stream<Arguments> provideSpecialItems() {
+        return Stream.of(
+            Arguments.of(AGED_BRIE, 1, 9, 21),
+            Arguments.of(AGED_BRIE, 10, 0, 30),
+            Arguments.of(AGED_BRIE, 11, -1, 32),
+            Arguments.of(AGED_BRIE, 100, -90, 50),
+
+            // Sulfuras never has to be sold and never decreases in Quality
+            Arguments.of(SULFURAS, 1, 10, 20),
+            Arguments.of(SULFURAS, 10, 10, 20),
+
+
+            Arguments.of(BACKSTAGE_PASSES, 1, 9, 22),
+            Arguments.of(BACKSTAGE_PASSES, 6, 4, 33),
+            Arguments.of(BACKSTAGE_PASSES, 11, -1, 0)
+        );
     }
 
 
